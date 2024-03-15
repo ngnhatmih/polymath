8b60aa5c2afd3ed969f46d086da27f93b845bdf9
ngnhatmih <ngnhatmih9997@gmail.com>
2024-03-15 23:05:09 +0700

    Quartz sync: Mar 15, 2024, 11:05 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -16,7 +16,13 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Breadcrumbs(),
+    Component.Breadcrumbs({
+      spacerSymbol: "►", // symbol between crumbs
+      rootName: "Home", // name of first/root element
+      resolveFrontmatterTitle: true, // whether to resolve folder names through frontmatter titles
+      hideOnRoot: true, // whether to hide breadcrumbs on root `index.md` page
+      showCurrentPage: true, // whether to display the current page in the breadcrumbs
+    }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
