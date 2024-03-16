c62c79e60674befb4b5e06404a1cc8d615697bde
ngnhatmih <ngnhatmih9997@gmail.com>
2024-03-16 13:31:15 +0700

    Quartz sync: Mar 16, 2024, 1:31 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -25,7 +25,7 @@ const config: QuartzConfig = {
         header: "Share Tech Mono",
         body: "Share Tech Mono",
         code: "Share Tech Mono",
-      },
+      }, 
       colors: {
         lightMode: {
           light: "#ffffff", // background
