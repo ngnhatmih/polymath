2638f28f9d53cb0212f23f3a2ee3e847eee1518e
ngnhatmih <ngnhatmih9997@gmail.com>
2024-03-25 12:51:54 +0700

    Quartz sync: Mar 25, 2024, 12:51 PM

--- quartz/plugins/transformers/codemirror.ts
+++ quartz/plugins/transformers/codemirror.ts
@@ -16,18 +16,24 @@ export const CodeMirror: QuartzTransformerPlugin = () => {
                             const code = node.value;
                             const customHtml: Html = {
                                 type: "html",
-                                value: `<div>
-                                <textarea id="editor">${code}</textarea>
-                                <pre id="output"></pre>
-                                <br /> 
-                                <button class="run-button">Run</button>
+                                value: `<div class="split">
+                                <div id="split-0">
+                                  <textarea id="editor">${code}</textarea>
                                 </div>
+                                <div id="split-1">
+                                  <button id="run">Run</button>
+                                  <pre id="canvas"></pre>
+                                </div>
+                              </div>
+                              <div class="console">
+                                <textarea id="output"></textarea>
+                            </div>
                                 `,
-                              }
-              
-                              const editor: (BlockContent | DefinitionContent)[] = [customHtml]
-              
-                              parent!.children.splice(index!, 1, ...editor)
+                            }
+
+                            const editor: (BlockContent | DefinitionContent)[] = [customHtml]
+
+                            parent!.children.splice(index!, 1, ...editor)
                         }
                     })
                 }
@@ -39,51 +45,147 @@ export const CodeMirror: QuartzTransformerPlugin = () => {
             const css: string[] = []
             const js: JSResource[] = []
             css.push(
-                "https://cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7/codemirror.min.css",
-                "https://cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7/theme/dracula.min.css"
-            );
-            
-            js.push(
-                {
-                    script: `
-                    document.addEventListener('nav', async () => {
-                        var myarea = document.getElementById('editor')
-                        var editor;
-                        if (myarea != null) {
-                            editor = CodeMirror.fromTextArea(myarea, {
-                                mode: "text/x-python",
-                                theme: "dracula",
-                                lineNumbers: true,
-                                indentUnit: 4,
-                                matchBrackets: true,
-                                textWrapping: false,
-                                height: "160px",
-                                fontSize: "9pt",
-                                autofocus: true,
-                            });
-                            editor.on("change", (cm) => {
-                                myarea.value = cm.getValue();
-                            });
-                        }
-                    })
+                "https://cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7/codemirror.css",
+                "https://cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7/theme/seti.css",
+                "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.23.0/addon/scroll/simplescrollbars.min.css"
+            ),
+
+                js.push(
+                    {
+                        script: `
+Split(["#split-0", "#split-1"]);
+
+var myeditor = document.getElementById("editor");
+var editor;
+if (myeditor != null) {
+  editor = CodeMirror.fromTextArea(myeditor, {
+    mode: "text/x-python",
+    theme: "seti",
+    indentUnit: 4,
+    lineWrapping: true,
+    scrollbarStyle: "overlay",
+    lineNumbers: true,
+    fontSize: "10pt"
+  });
+}
+editor.on("change", (cm) => {
+  myeditor.value = cm.getValue();
+});
+
+var myoutput = document.getElementById("output");
+var cm;
+if (myoutput != null) {
+  cm = CodeMirror.fromTextArea(myoutput, {
+    mode: "application/x-powershell",
+    readOnly: "nocursor",
+    theme: "seti",
+    indentUnit: 4,
+    lineNumbers: true,
+    lineWrapping: true,
+    scrollbarStyle: "overlay",
+    fontSize: "20pt",
+    viewportMargin: Infinity
+  });
+  
+  var externalLibs = {
+    "./numpy/__init__.js":
+        "https://cdn.jsdelivr.net/gh/ebertmi/skulpt_numpy@master/numpy/__init__.js",
+    "./numpy/random/__init__.js":
+        "https://cdn.jsdelivr.net/gh/ebertmi/skulpt_numpy@master/numpy/random/__init__.js"
+  };
+
+  function outf(text) {
+    cm.setValue(cm.getValue() + text);
+  }
+  
+  function builtinRead(file) {
+    console.log("Attempting file: " + Sk.ffi.remapToJs(file));
+  
+    if (externalLibs[file] !== undefined) {
+      return Sk.misceval.promiseToSuspension(
+        fetch(externalLibs[file]).then(function (resp) {
+          return resp.text();
+        })
+      );
+    }
+  
+    if (
+      Sk.builtinFiles === undefined ||
+      Sk.builtinFiles.files[file] === undefined
+    ) {
+      throw "File not found: '" + file + "'";
+    }
+  
+    return Sk.builtinFiles.files[file];
+  }
+  
+  function runit() {
+    var prog = document.getElementById("editor").value;
+    cm.setValue("");
+    Sk.configure({ output: outf, read: builtinRead });
+    (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = "canvas";
+    var myPromise = Sk.misceval.asyncToPromise(function () {
+      return Sk.importMainWithBody("<stdin>", false, prog, true);
+    });
+    myPromise.then(
+      function (mod) {
+        console.log("success");
+      },
+      function (err) {
+        console.log(err.toString());
+      }
+    );
+  }
+ document.addEventListener("nav", () => {
+    const button = document.getElementById("run");
+    if (button) {
+        button.addEventListener("click", (_e) => {
+            runit(); 
+        })
+    }
+});
+}
                     `,
-                    loadTime: "afterDOMReady",
-                    moduleType: "module",
-                    contentType: "inline",
-                },
-                // codemirror
-                {
-                    src: "https://cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7/codemirror.min.js",
-                    loadTime: "afterDOMReady",
-                    contentType: "external"
-                },
-                // language modes
-                {
-                    src: "https://cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7/mode/python/python.min.js",
-                    loadTime: "afterDOMReady",
-                    contentType: "external"
-                },
-            )
+                        loadTime: "afterDOMReady",
+                        moduleType: "module",
+                        contentType: "inline",
+                    },
+                    // codemirror
+                    {
+                        src: "https://cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7/codemirror.min.js",
+                        loadTime: "afterDOMReady",
+                        contentType: "external"
+                    },
+                    // language modes
+                    {
+                        src: "https://cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7/mode/python/python.min.js",
+                        loadTime: "afterDOMReady",
+                        contentType: "external"
+                    },
+                    // scrollbar
+                    {
+                        src: "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.23.0/addon/scroll/simplescrollbars.min.js",
+                        loadTime: "afterDOMReady",
+                        contentType: "external"
+                    },
+                    // split
+                    {
+                        src: "https://cdnjs.cloudflare.com/ajax/libs/split.js/1.6.2/split.js",
+                        loadTime: "afterDOMReady",
+                        contentType: "external"
+                    },
+                    //skuplt
+                    {
+                        src: "https://cdn.jsdelivr.net/npm/skulpt@1.2.0/dist/skulpt.min.js",
+                        loadTime: "afterDOMReady",
+                        contentType: "external"
+                    },
+                    {
+                        src: "https://cdn.jsdelivr.net/npm/skulpt@1.2.0/dist/skulpt-stdlib.min.js",
+                        loadTime: "afterDOMReady",
+                        contentType: "external"
+                    },
+                )
             return { js, css }
         }
     }
--- quartz/plugins/transformers/skulpt.ts
+++ quartz/plugins/transformers/skulpt.ts
@@ -8,59 +8,7 @@ export const Skulpt: QuartzTransformerPlugin = () => {
         name: "Skulpt",
         externalResources() {
             const js: JSResource[] = []
-            
-            js.push(
-                {
-                    script: `
-                    function outf(text) { 
-                        var mypre = document.getElementById("output"); 
-                        mypre.innerHTML = mypre.innerHTML + text; 
-                    }
-                    function builtinRead(x) {
-                        if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
-                                throw "File not found: '" + x + "'";
-                        return Sk.builtinFiles["files"][x];
-                    }
-                    function runit() { 
-                        var prog = document.getElementById("editor").value; 
-                        var mypre = document.getElementById("output");
-                        mypre.innerHTML = ''; 
-                        Sk.pre = "output";
-                        Sk.configure({output:outf, read:builtinRead}); 
-                        var myPromise = Sk.misceval.asyncToPromise(function() {
-                            return Sk.importMainWithBody("<stdin>", false, prog, true);
-                        });
-                        myPromise.then(function(mod) {
-                            console.log('success');
-                        },
-                            function(err) {
-                            console.log(err.toString());
-                        });
-                     }
-                     document.addEventListener("nav", () => {
-                        const button = document.querySelector(".run-button");
-                        if (button) {
-                            button.addEventListener("click", (_e) => {
-                                runit(); 
-                            })
-                        }
-                    });
-                    `,
-                    loadTime: "afterDOMReady",
-                    moduleType: "module",
-                    contentType: "inline"
-                },
-                {
-                    src: "https://cdn.jsdelivr.net/npm/skulpt@1.2.0/dist/skulpt.min.js",
-                    loadTime: "afterDOMReady",
-                    contentType: "external"
-                },
-                {
-                    src: "https://cdn.jsdelivr.net/npm/skulpt@1.2.0/dist/skulpt-stdlib.min.js",
-                    loadTime: "afterDOMReady",
-                    contentType: "external"
-                },
-            )        
+    
             return { js }
         }
     }
